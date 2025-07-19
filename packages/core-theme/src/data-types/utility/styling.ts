b5bce63fa41e891199d809e4f2e5e2aa7ad79c01
Tim VanderZwaag <tim@zilfire.com>
2025-07-19 13:27:53 -0400

    ...

--- packages/core-theme/src/data-types/utility/styling.ts
+++ packages/core-theme/src/data-types/utility/styling.ts
@@ -9,4 +9,10 @@ export type ThemeColor =
 
 export type FontStyle = 'normal' | 'italic';
 
-export type ColorMode = 'dark' | 'semi-dark' | 'normal' | 'dark-shade' | 'shade' | 'light-shade';
+export type ColorMode =
+  | 'darkest' //950 | 200 - The deepest, almost black.
+  | 'dark' //900 | 200 - Standard dark backgrounds.
+  | 'deep' //700 | 200- A deeper shade of color, used for emphasis.
+  | 'medium' //500 | 950 or 50 - Mid backbground color
+  | 'surface' //200 | 800 - Used for cards, sheets, or surfaces above the background.
+  | 'light'; //50 | 800 - Standard light backgrounds.
---
+++ packages/core-theme/src/web/utils/colorGrid.ts
@@ -0,0 +1,182 @@
+export const ColorGrid = {
+  darkest: {
+    primary: {
+      text: 'text-primary-200',
+      bg: 'bg-primary-950',
+    },
+    secondary: {
+      text: 'text-secondary-200',
+      bg: 'bg-secondary-950',
+    },
+    accent: {
+      text: 'text-accent-200',
+      bg: 'bg-accent-950',
+    },
+    'secondary-accent': {
+      text: 'text-secondary-accent-200',
+      bg: 'bg-secondary-accent-950',
+    },
+    neutral: {
+      text: 'text-neutral-200',
+      bg: 'bg-neutral-950',
+    },
+    white: {
+      text: 'text-black',
+      bg: 'bg-white',
+    },
+    black: {
+      text: 'text-white',
+      bg: 'bg-black',
+    },
+  },
+  dark: {
+    primary: {
+      text: 'text-primary-200',
+      bg: 'bg-primary-900',
+    },
+    secondary: {
+      text: 'text-secondary-200',
+      bg: 'bg-secondary-900',
+    },
+    accent: {
+      text: 'text-accent-200',
+      bg: 'bg-accent-900',
+    },
+    'secondary-accent': {
+      text: 'text-secondary-accent-200',
+      bg: 'bg-secondary-accent-900',
+    },
+    neutral: {
+      text: 'text-neutral-200',
+      bg: 'bg-neutral-900',
+    },
+    white: {
+      text: 'text-black',
+      bg: 'bg-white',
+    },
+    black: {
+      text: 'text-white',
+      bg: 'bg-black',
+    },
+  },
+  deep: {
+    primary: {
+      text: 'text-primary-200',
+      bg: 'bg-primary-700',
+    },
+    secondary: {
+      text: 'text-secondary-200',
+      bg: 'bg-secondary-700',
+    },
+    accent: {
+      text: 'text-accent-200',
+      bg: 'bg-accent-700',
+    },
+    'secondary-accent': {
+      text: 'text-secondary-accent-200',
+      bg: 'bg-secondary-accent-700',
+    },
+    neutral: {
+      text: 'text-neutral-200',
+      bg: 'bg-neutral-700',
+    },
+    white: {
+      text: 'text-black',
+      bg: 'bg-white',
+    },
+    black: {
+      text: 'text-white',
+      bg: 'bg-black',
+    },
+  },
+  medium: {
+    primary: {
+      text: 'text-primary-200',
+      bg: 'bg-primary-500',
+    },
+    secondary: {
+      text: 'text-secondary-200',
+      bg: 'bg-secondary-500',
+    },
+    accent: {
+      text: 'text-accent-200',
+      bg: 'bg-accent-500',
+    },
+    'secondary-accent': {
+      text: 'text-secondary-accent-200',
+      bg: 'bg-secondary-accent-500',
+    },
+    neutral: {
+      text: 'text-neutral-200',
+      bg: 'bg-neutral-500',
+    },
+    white: {
+      text: 'text-black',
+      bg: 'bg-white',
+    },
+    black: {
+      text: 'text-white',
+      bg: 'bg-black',
+    },
+  },
+  surface: {
+    primary: {
+      text: 'text-primary-200',
+      bg: 'bg-primary-200',
+    },
+    secondary: {
+      text: 'text-secondary-200',
+      bg: 'bg-secondary-200',
+    },
+    accent: {
+      text: 'text-accent-200',
+      bg: 'bg-accent-200',
+    },
+    'secondary-accent': {
+      text: 'text-secondary-accent-200',
+      bg: 'bg-secondary-accent-200',
+    },
+    neutral: {
+      text: 'text-neutral-200',
+      bg: 'bg-neutral-200',
+    },
+    white: {
+      text: 'text-black',
+      bg: 'bg-white',
+    },
+    black: {
+      text: 'text-white',
+      bg: 'bg-black',
+    },
+  },
+  light: {
+    primary: {
+      text: 'text-primary-200',
+      bg: 'bg-primary-50',
+    },
+    secondary: {
+      text: 'text-secondary-200',
+      bg: 'bg-secondary-50',
+    },
+    accent: {
+      text: 'text-accent-200',
+      bg: 'bg-accent-50',
+    },
+    'secondary-accent': {
+      text: 'text-secondary-accent-200',
+      bg: 'bg-secondary-accent-50',
+    },
+    neutral: {
+      text: 'text-neutral-200',
+      bg: 'bg-neutral-50',
+    },
+    white: {
+      text: 'text-black',
+      bg: 'bg-white',
+    },
+    black: {
+      text: 'text-white',
+      bg: 'bg-black',
+    },
+  },
+};
