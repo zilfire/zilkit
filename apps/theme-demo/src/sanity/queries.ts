e5844d77858d54983b70236c8be0737455a18502
Tim VanderZwaag <tim@zilfire.com>
2025-09-14 10:59:44 -0400

    ...

--- apps/theme-demo/src/sanity/queries.ts
+++ apps/theme-demo/src/sanity/queries.ts
@@ -1,8 +1,8 @@
 import { defineQuery, SanityDocument } from 'next-sanity';
 import type { HeroBlockData } from '@zilfire/core-theme/data-types';
 
-export const HOME_QUERY = defineQuery(`*[_type == "homePage" && _id == "siteHome"][0]`);
-
 export type HomeQueryData = SanityDocument & {
   hero?: HeroBlockData;
 };
+
+export const HOME_QUERY = defineQuery(`*[_type == "homePage" && _id == "siteHome"][0]`);
--- packages/core-theme/src/web/blocks/HeroBlock.tsx
+++ packages/core-theme/src/web/blocks/HeroBlock.tsx
@@ -4,7 +4,9 @@ type HeroBlockProps = {
   data: HeroBlockData;
 };
 
-export const HeroBlock: React.FC<HeroBlockProps> = ({ data: { heading, description } }) => {
+export const HeroBlock: React.FC<HeroBlockProps> = ({
+  data: { heading, description, backgroundImage },
+}) => {
   return (
     <div className="bg-gray-300 p-8">
       <h2 className="text-2xl font-bold mb-4">{heading}</h2>
