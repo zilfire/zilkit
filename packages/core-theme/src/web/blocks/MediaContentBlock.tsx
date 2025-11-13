45dce8bc9ebc97179075091f5e38a9fa93c08563
Tim VanderZwaag <tim@zilfire.com>
2025-11-13 10:44:07 -0500

    ...

--- packages/core-theme/src/web/blocks/MediaContentBlock.tsx
+++ packages/core-theme/src/web/blocks/MediaContentBlock.tsx
@@ -2,6 +2,11 @@ import type { MediaContentBlockData } from '../../types/index.js';
 import type { ThemeContext } from '../../types/context-types/index.js';
 import { Section } from '../components/Section.js';
 import { H2, Span } from '../text/index.js';
+import SanityImage from '@zilfire/next-sanity-image';
+import { textComponents } from '../text/text-components.js';
+import { PortableText } from 'next-sanity';
+import { getHorizontalGapSpacingClass } from '../style/style-utils/layout-style-utils.js';
+import clsx from 'clsx';
 
 export interface MediaContentBlockProps {
   data?: MediaContentBlockData;
@@ -11,10 +16,22 @@ export interface MediaContentBlockProps {
 export const MediaContentBlock: React.FC<MediaContentBlockProps> = ({ data, context }) => {
   if (!data) return null;
   const { image, heading, subheading, eyebrow, content } = data;
+
+  const gapClass = getHorizontalGapSpacingClass('lg', context.styleClasses);
+
   return (
     <Section context={context}>
-      {eyebrow && <Span>{eyebrow}</Span>}
-      {heading && <H2>{heading}</H2>}
+      <div className={clsx('flex flex-wrap lg:flex-nowrap', 'items-center', gapClass)}>
+        <div className="w-full lg:w-1/2">
+          {image && <SanityImage imageObject={image} sanityConfig={context.sanityConfig} />}
+        </div>
+        <div className="w-full lg:w-1/2">
+          {eyebrow && <Span>{eyebrow}</Span>}
+          {heading && <H2>{heading}</H2>}
+          {subheading && <Span>{subheading}</Span>}
+          {content && <PortableText value={content} components={textComponents({}, context)} />}
+        </div>
+      </div>
     </Section>
   );
 };
