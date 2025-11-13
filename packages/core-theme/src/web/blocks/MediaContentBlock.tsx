f4074dff529ef3d84d3535265af1b62b24ae2fb9
Tim VanderZwaag <tim@zilfire.com>
2025-11-13 11:42:21 -0500

    ...

--- packages/core-theme/src/web/blocks/MediaContentBlock.tsx
+++ packages/core-theme/src/web/blocks/MediaContentBlock.tsx
@@ -1,12 +1,16 @@
 import type { MediaContentBlockData } from '../../types/index.js';
 import type { ThemeContext } from '../../types/context-types/index.js';
 import { Section } from '../components/Section.js';
-import { H2, Span } from '../text/index.js';
+import { H2, H3, H4, Span, P } from '../text/index.js';
 import SanityImage from '@zilfire/next-sanity-image';
 import { textComponents } from '../text/text-components.js';
 import { PortableText } from 'next-sanity';
-import { getHorizontalGapSpacingClass } from '../style/style-utils/layout-style-utils.js';
+import {
+  getHorizontalGapSpacingClass,
+  getColumnLayoutClass,
+} from '../style/style-utils/layout-style-utils.js';
 import clsx from 'clsx';
+// import { verticalLineSpacing } from '../style/button-classes.js';
 
 export interface MediaContentBlockProps {
   data?: MediaContentBlockData;
@@ -18,17 +22,26 @@ export const MediaContentBlock: React.FC<MediaContentBlockProps> = ({ data, cont
   const { image, heading, subheading, eyebrow, content } = data;
 
   const gapClass = getHorizontalGapSpacingClass('lg', context.styleClasses);
+  const columnClass = getColumnLayoutClass('half', context.styleClasses);
 
   return (
     <Section context={context}>
       <div className={clsx('flex flex-wrap lg:flex-nowrap', 'items-center', gapClass)}>
-        <div className="w-full lg:w-1/2">
+        <div className={clsx(columnClass)}>
           {image && <SanityImage imageObject={image} sanityConfig={context.sanityConfig} />}
         </div>
-        <div className="w-full lg:w-1/2">
-          {eyebrow && <Span>{eyebrow}</Span>}
-          {heading && <H2>{heading}</H2>}
-          {subheading && <Span>{subheading}</Span>}
+        <div className={clsx(columnClass)}>
+          {eyebrow && (
+            <Span className="uppercase" styleOptions={{ color: 'muted' }}>
+              {eyebrow}
+            </Span>
+          )}
+          {heading && (
+            <H2 styleOptions={{}} classOverrides={{ verticalSpacing: '' }}>
+              {heading}
+            </H2>
+          )}
+          {subheading && <H4 styleOptions={{}}>{subheading}</H4>}
           {content && <PortableText value={content} components={textComponents({}, context)} />}
         </div>
       </div>
